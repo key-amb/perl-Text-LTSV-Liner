3d4cea29e71913202838ab69afce718bcbfaa8c3
key-amb <yasutake.kiyoshi@gmail.com>
2013-06-19 12:18:40 -0400

    generated by minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Text::LTSV::Liner - It's new $module
+
+# SYNOPSIS
+
+    use Text::LTSV::Liner;
+
+# DESCRIPTION
+
+Text::LTSV::Liner is ...
+
+# LICENSE
+
+Copyright (C) key-amb.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+key-amb <yasutake.kiyoshi@gmail.com>
---
+++ lib/Text/LTSV/Liner.pm
@@ -0,0 +1,39 @@
+package Text::LTSV::Liner;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Text::LTSV::Liner - It's new $module
+
+=head1 SYNOPSIS
+
+    use Text::LTSV::Liner;
+
+=head1 DESCRIPTION
+
+Text::LTSV::Liner is ...
+
+=head1 LICENSE
+
+Copyright (C) key-amb.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+key-amb E<lt>yasutake.kiyoshi@gmail.comE<gt>
+
+=cut
+
